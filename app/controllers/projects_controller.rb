af0c4f447d186c4926543eff88a1c698d9346b78
justine gauthier <gauthier.justine25000@gmail.com>
2022-07-01 11:57:41 +0200

    form d'edit et de new

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: %i[show edit update destroy authorize]
-  before_action :authorized, only: %i[new create edit update destroy]
+  before_action :set_project, only: %i[show edit update destroy authorized]
+  before_action :authorized, only: %i[edit update destroy]
 
   def show; end
 
@@ -10,15 +10,23 @@ class ProjectsController < ApplicationController
 
   def new
     @project = Project.new
+    authorize @project
   end
 
   def create
     @project = Project.new(project_params)
+    authorize @project
+    if @project.save!
+      redirect_to root_path
+    else
+      render :new, notice: 'Oups, il y a eu un petit problème...'
+    end
   end
 
   def edit
     @categories_instances = Categorie.all
     @categories_names = @categories_instances.map(&:name)
+    @categories_ids = @categories_instances.map(&:id)
   end
 
   def update
@@ -34,7 +42,7 @@ class ProjectsController < ApplicationController
   private
 
   def project_params
-    params.require(:project).permit(:title, :description, :url, :creation_date, :categorie)
+    params.require(:project).permit(:title, :description, :url, :creation_date, :categorie_id)
   end
 
   def set_project

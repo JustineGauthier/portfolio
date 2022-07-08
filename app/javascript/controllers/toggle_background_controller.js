a8c28f1d2af8ce8181f738cce26523158e1259bb
justine gauthier <gauthier.justine25000@gmail.com>
2022-07-08 16:53:32 +0200

    delete stimulus

--- app/javascript/controllers/toggle_background_controller.js
+++ app/javascript/controllers/toggle_background_controller.js
@@ -3,7 +3,7 @@ export default class extends Controller {
   static targets = ["input"]
 
   background() {
-    console.log('coucou')
-    // this.backgroundTarget.classList.toggle("d-none")
+    // console.log('coucou')
+    this.backgroundTarget.classList.toggle("body", "d-none")
   }
 }

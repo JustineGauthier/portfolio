03d5bc76e162449d6159ac87f0ef232b2e22dee5
justine gauthier <gauthier.justine25000@gmail.com>
2022-07-08 17:40:14 +0200

    change background toggle

--- app/javascript/controllers/toggle_background_controller.js
+++ app/javascript/controllers/toggle_background_controller.js
@@ -1,9 +1,7 @@
 import { Controller } from "stimulus"
 export default class extends Controller {
-  static targets = ["input"]
 
   background() {
-    // console.log('coucou')
-    this.backgroundTarget.classList.toggle("body", "d-none")
+    this.element.classList.toggle("blue")
   }
 }
